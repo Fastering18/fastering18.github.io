ec348fe0748b18c5b1d60e98a47d7ebb6ba17af4
Fastering18 <73803835+Fastering18@users.noreply.github.com>
2025-05-18 10:10:50 +0000

    Deploying to gh-pages from @ Fastering18/fastering18.github.io@939c65edf5f1c0e239b4653b8aac290ce9208dba 🚀

--- _next/static/WRQcAsHx2wypkn8i8-FD8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-bb1cf9d696c6c5d3.js","static/chunks/246-262684bebc0968a8.js","static/chunks/pages/index-200bd43ce6997750.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
