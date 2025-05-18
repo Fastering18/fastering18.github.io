0b0459dc65084394af554f10cd606948ac5527da
Fastering18 <brahmana165@gmail.com>
2025-05-18 17:22:35 +0700

    work

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/fastering18.github.io' : '',
+  //basePath: process.env.NODE_ENV === 'production' ? '/fastering18.github.io' : '',
 }
 
 module.exports = nextConfig 
